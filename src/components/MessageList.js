aa0367f54b80717a8c0e28db4dcf0bc6fc11e9f9
Linda Yang <lindayang@Lindas-iMac.local>
2018-12-28 04:51:24 -0500

    Deleted DUMMY_DATA and passed down props

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
 
-const DUMMY_DATA = [
-  {
-    senderId: 'admin',
-    text: 'Hey, how is it going?'
-  },
-  {
-    senderId: 'root',
-    text: 'Great! How about you?'
-  },
-  {
-    senderId: 'admin',
-    text: 'Good to hear: I am great as well'
-  }
-]
-
 class MessageList extends Component {
   render() {
     return (
       <div className='message-list'>
-        {DUMMY_DATA.map((message, index) => {
+        {this.props.messages.map((message, index) => {
           return (
               <div key={index} className='message'>
                 <div className='message-username'>{message.senderId}</div>
