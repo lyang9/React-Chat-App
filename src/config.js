868405a04a41dfa4363dbe9a617dd4aadb04e94f
Linda Yang <lindayang@Lindas-iMac.local>
2018-12-28 03:21:41 -0500

    Exported tokenUrl

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 const instanceLocator = 'v1:us1:9d6718ca-f8de-4ec9-8fdc-55a28367f3a0';
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/9d6718ca-f8de-4ec9-8fdc-55a28367f3a0/token';
 
-export { instanceLocator }
+export { instanceLocator, tokenUrl }
