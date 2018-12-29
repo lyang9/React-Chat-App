b866007e2a5331f0b0de1e3b413f1389e8288e1d
Linda Yang <lindayang@Lindas-iMac.local>
2018-12-29 02:41:59 -0500

    Created form and added funtionality

---
+++ src/components/SendMessageForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class SendMessageForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state.message)
+  }
+
+  render() {
+    return (
+      <form 
+        className='send-message-form'
+        onSubmit={this.handleSubmit}
+      >
+        <input 
+          type='text'
+          placeholder='Type your message and hit ENTER'
+          onChange={this.handleChange}
+          value={this.state.message}
+        />
+      </form>
+    );
+  }
+}
+
+export default SendMessageForm;
